5e312c5e7c05d86d9d502e68b224e0fc5e1c1586
Gilaric <pkg.thomsen@gmail.com>
2024-03-05 08:50:41 +0100

    Added tests. Not working, but setup for coding.

---
+++ BlazorTestApp.Tests/ExternalLoginTests/ExternalLoginTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTestApp.Tests.ExternalLoginTests
+{
+    public class LoginTests : TestContext
+    {
+        [Fact]
+        public void Test()
+        {
+            // Arrange
+            var authContext = this.AddTestAuthorization();
+            authContext.SetAuthorized("TEST USER");
+
+            // Act
+            var cut = RenderComponent<Login>();
+
+            // Assert
+            cut.MarkupMatches(@"<h1>Welcome TEST USER</h1>
+                      <p>State: Authorized</p>");
+        }
+    }
+}
