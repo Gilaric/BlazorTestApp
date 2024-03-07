7307f119abc31d2312494c4461f29dfe2ab80950
Gilaric <pkg.thomsen@gmail.com>
2024-03-07 11:59:32 +0100

    Attempted Register

--- BlazorTestApp.Tests/FileCreateTest.cs
+++ BlazorTestApp.Tests/FileCreateTest.cs
@@ -30,6 +30,45 @@ namespace BlazorTestApp.Tests
 
 			// Assertions...
 			Assert.NotNull(inputFile);
+            var smallElm = cut.Find("p.cl_FileSuccess");
+            smallElm.MarkupMatches(@"<p class=""cl_FileSuccess"">Files successfully uploaded</p>");
         }
-	}
+
+        [Fact]
+        public void FailedCreateTest()
+        {
+            // Create BUnit for testing if a file is created
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Create an array of InputFileContent with more than 3 files
+            InputFileContent[] filesToUpload = new InputFileContent[]
+            {
+                InputFileContent.CreateFromText("Text content 1", "File1.txt"),
+                InputFileContent.CreateFromText("Text content 2", "File2.txt"),
+                InputFileContent.CreateFromText("Text content 3", "File3.txt"),
+                InputFileContent.CreateFromText("Text content 4", "File4.txt"),
+                InputFileContent.CreateFromText("Text content 5", "File5.txt")
+            };
+
+            // Render the component under test which contains the InputFile component as a child component
+            IRenderedComponent<FileCreate> cut = ctx.RenderComponent<FileCreate>();
+
+            // Find the InputFile component
+            IRenderedComponent<InputFile> inputFile = cut.FindComponent<InputFile>();
+
+            inputFile.UploadFiles(filesToUpload);
+
+            // Assertions...
+            Assert.NotEmpty(filesToUpload);
+
+            Assert.InRange(filesToUpload.Length, 4, 5);
+
+            var FailElm2 = cut.Find("h2");
+            FailElm2.MarkupMatches(@"<h2>Errors</h2>");
+
+            var FailElm = cut.Find("p.cl_FileFailed");
+            FailElm.MarkupMatches(@"<p class=""cl_FileFailed"">File not uploaded</p>");
+        }
+    }
 }
