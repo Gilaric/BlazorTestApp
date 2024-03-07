0a33e9d8a5acae22ce0c5b7b750b3ed971d4e1d4
Gilaric <pkg.thomsen@gmail.com>
2024-03-07 12:08:55 +0100

    Deleted tests not needed

--- BlazorTestApp.Tests/RegisterTests.cs
+++ BlazorTestApp.Tests/RegisterTests.cs
@@ -1,37 +1,38 @@
 ﻿using BlazorTestApp.Components.Account.Pages;
 using Microsoft.AspNetCore.Components.Forms;
+using BlazorTestApp.Components.Account;
 using System;
 
 namespace BlazorTestApp.Tests
 {
     public class RegisterTests
     {
-        [Fact]
-        public void RegisterSuccessTest()
-        {
-            // Create BUnit for testing if a file is created
-            // Arrange
-            using var ctx = new TestContext();
-            // Mock RedirectManager service
+        //[Fact]
+        //public void RegisterSuccessTest()
+        //{
+        //    // Create BUnit for testing if a file is created
+        //    // Arrange
+        //    using var ctx = new TestContext();
+        //    // Mock RedirectManager service
 
-            // Add other required services if needed
-            /*ctx.Services.AddSingleton<IdentityRedirectManager>();*/ // Register IdentityRedirectManager
+        //    // Add other required services if needed
+        //    /*ctx.Services.AddSingleton<IdentityRedirectManager>(); */// Register IdentityRedirectManager
 
-            // Act
-            var cut1 = ctx.RenderComponent<Register>();
-            Random rnd = new Random();
+        //    // Act
+        //    var cut1 = ctx.RenderComponent<Register>();
+        //    Random rnd = new Random();
 
-            // Find the InputText components within the form
-            var inputEmail = cut1.Find("div.form-floating.mb-3 input[type='email']");
-            var inputPassword = cut1.Find("div.form-floating.mb-3 input[type='password']");
+        //    // Find the InputText components within the form
+        //    var inputEmail = cut1.Find("div.form-floating.mb-3 input[type='email']");
+        //    var inputPassword = cut1.Find("div.form-floating.mb-3 input[type='password']");
 
-            inputEmail.Change($"pkg.thomsen{rnd.Next()}@gmail.com");
-            inputPassword.Change("password123");
+        //    inputEmail.Change($"pkg.thomsen{rnd.Next()}@gmail.com");
+        //    inputPassword.Change("password123");
 
-            // Assert
-            // You can optionally assert that the values have been changed
-            Assert.Equal("pkg.thomsen@gmail.com", inputEmail.GetAttribute("value"));
-            Assert.Equal("password123", inputPassword.GetAttribute("value"));
-        }
+        //    // Assert
+        //    // You can optionally assert that the values have been changed
+        //    Assert.Equal("pkg.thomsen@gmail.com", inputEmail.GetAttribute("value"));
+        //    Assert.Equal("password123", inputPassword.GetAttribute("value"));
+        //}
     }
 }
