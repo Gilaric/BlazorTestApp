a8ed8744aff694124fe2679abb2e2f0924319f4f
Gilaric <pkg.thomsen@gmail.com>
2024-04-05 11:58:27 +0200

    Routes needs fixing

---
+++ APIs/Controllers/TodoListsController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlazorTestApp.Data;
+using BlazorTestApp.Models;
+
+namespace APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoListsController : ControllerBase
+    {
+        private readonly TodoDbContext _context;
+
+        public TodoListsController(TodoDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoLists
+        [HttpGet]
+        [Route("{medicId}")]
+        public async Task<ActionResult<IEnumerable<TodoList>>> GetTodoLists()
+        {
+            return await _context.TodoLists.ToListAsync();
+        }
+
+        // GET: api/TodoLists/5
+        [HttpGet("{id}")]
+        [Route("{medicId}")]
+        public async Task<ActionResult<TodoList>> GetTodoList(int id)
+        {
+            var todoList = await _context.TodoLists.FindAsync(id);
+
+            if (todoList == null)
+            {
+                return NotFound();
+            }
+
+            return todoList;
+        }
+
+        // PUT: api/TodoLists/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        [Route("{medicId}")]
+        public async Task<IActionResult> PutTodoList(int id, TodoList todoList)
+        {
+            if (id != todoList.ToDoListId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoList).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoListExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoLists
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [Route("{medicId}")]
+        public async Task<ActionResult<TodoList>> PostTodoList(TodoList todoList)
+        {
+            _context.TodoLists.Add(todoList);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodoList", new { id = todoList.ToDoListId }, todoList);
+        }
+
+        // DELETE: api/TodoLists/5
+        [HttpDelete("{id}")]
+        [Route("{medicId}")]
+        public async Task<IActionResult> DeleteTodoList(int id)
+        {
+            var todoList = await _context.TodoLists.FindAsync(id);
+            if (todoList == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoLists.Remove(todoList);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoListExists(int id)
+        {
+            return _context.TodoLists.Any(e => e.ToDoListId == id);
+        }
+    }
+}
